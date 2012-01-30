c1f0a6bb43ef06d78f56cef9fedf1746a576da0f
Esa-Matti Suuronen <esa-matti.suuronen@opinsys.fi>
2012-01-30 13:11:01 +0000

    Attach ShareJS to the example server.

--- examples/todos/server.js
+++ examples/todos/server.js
@@ -1,10 +1,18 @@
 
 var express = require("express");
+var sharejs = require("share").server;
+
 var app = express.createServer();
+
+sharejs.attach(app, {
+  db:{ type: "none" }
+});
 
 app.configure('development', function(){
   app.use(express.static(__dirname + '/public'));
 });
 
 
-app.listen(3000);
+app.listen(3000, function() {
+  console.log("Now listening on http://localhost:3000/");
+});
